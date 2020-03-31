875f2dededfa6154453aa97a3b3c56ad0b2e3a78
novianurulumami <nummynovia12@gmail.com>
2020-03-31 22:57:32 +0700

    merubah content

--- resources/views/admin/datasiswa/index.blade.php
+++ resources/views/admin/datasiswa/index.blade.php
@@ -1,4 +1,40 @@
 @extends('layouts.admin')
 @section('content')
-<h3>Data Siswa</h3>
+<div class="container-fluid">
+    <div class="row">
+        <h3>Data Siswa</h3>
+        <div class="col-4">
+            <a href="#" class="btn btn-info active" role="button" aria-pressed="true">Tambah Data</a>
+            <a href="#" class="btn btn-info active" role="button" aria-pressed="true">Edit Data Kelas</a>
+            <a href="#" class="btn btn-info active" role="button" aria-pressed="true">Import Data</a>
+            <a href="#" class="btn btn-info active" role="button" aria-pressed="true">Export Data</a>
+            <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown button
+            </button>
+                <ul class="dropdown-menu dropdown-user">
+                <li><a href="#">Akuntansi</a>
+                </li>
+                <li><a href="#">Multimedia</a>
+                </li>
+                <li><a href="#">Pemasaran</a>
+                </li>
+                <li><a href="#">Perkantoran</a>
+                </li>
+                </ul>
+            </div>
+            <div class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </input>
+                        </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Admin</title>
+    <title>SMK PGRI ROGOJAMPI</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="{{asset('css/libs3.css')}}" rel="stylesheet">
@@ -49,31 +49,7 @@
         <ul class="nav navbar-top-links navbar-right">
 
 
-            <!-- /.dropdown -->
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-user">
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                    </li>
-                    <li class="divider"></li>
-                    <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                    </li>
-                </ul>
-                <!-- /.dropdown-user -->
-            </li>
-            <!-- /.dropdown -->
+            
 
 
         </ul>
@@ -107,10 +83,10 @@
 
 
 
-        <div class="navbar-default sidebar" role="navigation">
+        <div class="navbar-default sidebar navbar-static-top" role="navigation">
             <div class="sidebar-nav navbar-collapse">
                 <ul class="nav" id="side-menu">
-                    <li class="sidebar-search">
+                    <!-- <li class="sidebar-search">
                         <div class="input-group custom-search-form">
                             <input type="text" class="form-control" placeholder="Search...">
                                 <span class="input-group-btn">
@@ -119,8 +95,34 @@
                                     </button>
                                 </span>
                         </div>
-                        <!-- /input-group -->
+                        /input-group
+                    </li> -->
+                    
+                    <!-- /.dropdown -->
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                            </li>
+                            <li class="divider"></li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                            </li>
+                        </ul>
+                        <!-- /.dropdown-user -->
                     </li>
+                    <!-- /.dropdown -->
                     <li>
                         <a href="{{ url('/dashboard') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                     </li>
