63a657ae2b445d7c6fa15092c93f375c5bd0d4bd
novianurulumami <nummynovia12@gmail.com>
2020-04-07 03:03:33 +0700

    setoran dan penarikan

--- resources/views/admin/penarikan/index.blade.php
+++ resources/views/admin/penarikan/index.blade.php
@@ -23,10 +23,22 @@
 </div>
 
 <div class="form-group row">
-    <label for="email" class="col-md-2 col-form-label font-weight-normal"><h4>Jurnal</h4></label>
-    <div class="col-md-6">
-        <input id="" type="email" class="form-control " name="email" value="" required autocomplete="email" autofocus>
-    </div>
+<label for="email" class="col-md-2 col-form-label font-weight-normal"><h4>Jurnal</h4></label>
+<div class="col-md-2">
+    <select id="" type="email" class="form-control " name="email" value="" required autocomplete="email" autofocus>
+        <option value="111">Kas</option>
+        <option value="211">Hutang</option>
+    </select>
+</div>
+<div class="col-md-1">
+<a href="#"><i class="fa fa-exchange"></i></a>
+</div>
+<div class="col-md-2">
+    <select id="" type="email" class="form-control " name="email" value="" required autocomplete="email" autofocus>
+        <option value="111">Kas</option>
+        <option value="211">Hutang</option>
+    </select>
+</div>
 </div>
 
 <div class="form-group row">
