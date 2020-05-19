2c7bf2519505607994748a87240af26324652474
novianurulumami <nummynovia12@gmail.com>
2020-05-19 23:01:26 +0700

    penambahan pagination

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -96,7 +96,22 @@
             <tbody>
             </tbody>
           </table>
-        </div>
+        </div><nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+
         <button type='button' class='btn btn-primary center-block'> Cetak </button>
 <script type="text/javascript">
 
